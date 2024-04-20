41e53dc29f7dede7e5dbcf6924d7aea46c024606
dutrieum <dutrieuxmathilde@gmail.com>
2024-04-20 17:42:41 +0200

    feat: add png pictos

--- components/feed/PlayerStatistics.js
+++ components/feed/PlayerStatistics.js
@@ -4,9 +4,9 @@ import Statistic from "./Statistic"
 export default PlayerStatistics = ({ data }) => {
   return (
     <View className="flex-row justify-between">
-      <Statistic progress={66} data="6M" />
-      <Statistic progress={45} data="3M" />
-      <Statistic progress={20} data="20%" />
+      <Statistic type="reputation" progress={66} data="6M" />
+      <Statistic type="followers" progress={45} data="3M" />
+      <Statistic type="money" progress={20} data="20%" />
     </View>
   )
 }
--- components/feed/Statistic.js
+++ components/feed/Statistic.js
@@ -1,12 +1,42 @@
-import { View } from "react-native"
+import { Image, StyleSheet, View } from "react-native"
 import Text from "../typography/Text"
 import ProgressCircle from "./ProgressCircle"
 
-export default Statistic = ({ progress, data }) => {
+export default Statistic = ({ progress, data, type }) => {
+  let picto;
+  switch (type) {
+    case 'reputation':
+      picto = require('../../assets/picto/reputation.png');
+      break;
+    case 'followers':
+      picto = require('../../assets/picto/followers.png');
+      break;
+    case 'money':
+      picto = require('../../assets/picto/money.png');
+      break;
+    default:
+      break;
+  }
+
   return (
-    <View className="flex-row items-center gap-7">
+    <View className="flex-row items-center gap-7 relative">
       <ProgressCircle progress={progress} />
+      <Image style={styles.picto} source={picto} />
       <Text>{data}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  picto: {
+    width: 24,
+    height: 24,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    transform: [
+      {translateX: 24},
+      {translateY: 24}
+    ]
+  }
+});
