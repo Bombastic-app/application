5bc35907d9dd3774d5122216d8a8483f76e75080
charlottedrb <c.derbaghdassarian@gmail.com>
2024-04-22 15:13:40 +0200

    feat: edit colors

--- components/typography/Text.js
+++ components/typography/Text.js
@@ -2,6 +2,6 @@ import { Text as BaseText } from "react-native";
 
 export default Text = ({ className, style, children }) => {
   return (
-    <BaseText className={`text-beige text-14 ${className}`} style={style}>{ children }</BaseText>
+    <BaseText className={`text-white text-14 ${className}`} style={style}>{ children }</BaseText>
   )
 }
