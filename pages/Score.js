635dead905ef8827d5e376db8acf84ea4c01f03e
dutrieum <dutrieuxmathilde@gmail.com>
2024-04-24 13:55:28 +0200

    feat: update certif png

--- pages/Score.js
+++ pages/Score.js
@@ -5,27 +5,15 @@ import { RoundedButton } from "../components/base/RoundedButton";
 import { Button, Image, StyleSheet, View } from "react-native";
 import { useRouter } from "expo-router";
 import { CONSTANTS } from "../constants";
-import { useDispatch, useSelector } from "react-redux";
-import { upgradeScore, downgradeScore, updateScore } from "../store";
+import { useSelector } from "react-redux";
 
 export default Score = () => {
   const router = useRouter();
-  const dispatch = useDispatch();
   const score = useSelector(state => state.score);
 
   const [ certifHeight, setCertifHeight] = useState(0);
   const handleOnLayout = (e) => {
     setCertifHeight(e.nativeEvent.layout.height);
-  };
-
-  const handleOnUpgrade = () => {
-    dispatch(upgradeScore())
-  };
-  const handleOnDowngrade = () => {
-    dispatch(downgradeScore())
-  };
-  const handleOnUpdate = () => {
-    dispatch(updateScore(1))
   };
 
   return (
@@ -35,16 +23,12 @@ export default Score = () => {
           <Text className="uppercase font-balgin-narrow-bold text-18 text-center">Bravo, tu as</Text>
           <Text className="font-balgin-black text-56 text-center pb-24 pt-16">Gagné !</Text>
           <Text className="text-center">Plus que {CONSTANTS.maxPoints - score} paliers avant d’être une star !</Text>
-
-          <Button onPress={handleOnUpgrade} title="Augmente le score" />
-          <Button onPress={handleOnDowngrade} title="Baisse le score" />
-          <Button onPress={handleOnUpdate} title="Définit le score à 1" />
         </View>
 
         <View style={[styles.certifContainer, {transform: [{translateY: -(certifHeight / 2)}],}]}>
           <Image style={styles.certifImage} source={require("../assets/certif.png")} onLayout={handleOnLayout} />
 
-            <View className="absolute bottom-50">
+            <View className="absolute bottom-0 translate-y-60">
               <Text className="text-32 font-balgin-narrow-bold uppercase">{score} point</Text>
             </View>
         </View>
@@ -78,6 +62,6 @@ const styles = StyleSheet.create({
   certifImage: {
     width: '100%',
     objectFit: 'contain',
-    height: 470
+    height: 190
   }
 });
