5ff1256269f3a3c863cab833ae19150a153ad4e0
dutrieum <dutrieuxmathilde@gmail.com>
2024-06-14 15:25:23 +0200

    fix: keyboard cutting single post

--- pages/SinglePost.js
+++ pages/SinglePost.js
@@ -72,7 +72,7 @@ export default SinglePost = ({ type, content, pseudo, author, currentTurn, likes
         <BackArrow />
       </Pressable>
 
-      <KeyboardAwareScrollView contentContainerStyle={{ flex: 1 }} bounces={false} scrollEnabled={false}>
+      <KeyboardAwareScrollView contentContainerStyle={{ flex: 1 }} bounces={true} scrollEnabled={false} automaticallyAdjustContentInsets={false}>
 
         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
           <Post type={type} content={content} pseudo={pseudo} author={author} soloView={true} withBackground={false} likes={likes} dislikes={dislikes} />
