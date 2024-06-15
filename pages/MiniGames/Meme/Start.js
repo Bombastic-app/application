c00b01cb0e12e83de607f4b9c56eca291fbb5473
dutrieum <dutrieuxmathilde@gmail.com>
2024-06-15 16:22:16 +0200

    feat: update mini game start screen

--- pages/MiniGames/Meme/Start.js
+++ pages/MiniGames/Meme/Start.js
@@ -1,8 +1,10 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
-import { Image } from "expo-image";
 import BaseScreen from "../../../components/base/BaseScreen";
+import { LinearGradient } from "expo-linear-gradient";
+import { colors } from "../../../components/Style";
+import CardTitle from "../../../components/turn/CardTitle";
 
 export default Start = () => {
   const router = useRouter();
@@ -15,14 +17,25 @@ export default Start = () => {
 
   return (
     <BaseScreen headerShown={false}>
-      <View className="flex-1 justify-center items-center">
-        <Image
-          source={require("../../../assets/ref.png")}
-          contentFit="contain"
-          cachePolicy={"memory-disk"}
-          style={{width: '100%', height: '100%'}}
-        />
+      <LinearGradient
+        start={{ x: 1, y: 1 }}
+        end={{ x: 1, y: 0 }}
+        style={styles.gradient}
+        colors={[colors.blue, colors.purple, colors.pink]}
+      />
+      <View className="flex-1 item-center justify-center">
+        <CardTitle title="Deviens une ref" />
       </View>
     </BaseScreen>
   )
 }
+
+const styles = StyleSheet.create({
+  gradient: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
